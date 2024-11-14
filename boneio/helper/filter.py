48a5858c7acc7906b5782db7efc2c490ef8c392e
pszafer <pszafer@gmail.com>
2024-11-14 22:49:40 +0100

    fix filter run twice for energy meters

--- boneio/helper/filter.py
+++ boneio/helper/filter.py
@@ -1,6 +1,7 @@
 """Filter class to adjust sensor values."""
 
 from __future__ import annotations
+from typing import Optional
 import logging
 
 _LOGGER = logging.getLogger(__name__)
@@ -9,7 +10,7 @@ _LOGGER = logging.getLogger(__name__)
 FILTERS = {
     "offset": lambda x, y: x + y,
     "round": lambda x, y: round(x, y),
-    "multiply": lambda x, y: x * y,
+    "multiply": lambda x, y: x * y if x else x,
     "filter_out": lambda x, y: None if x == y else x,
     "filter_out_greater": lambda x, y: None if x > y else x,
     "filter_out_lower": lambda x, y: None if x < y else x,
@@ -19,8 +20,10 @@ FILTERS = {
 class Filter:
     _filters = []
 
-    def _apply_filters(self, value: float, filters: list = []) -> float | None:
-        filters = filters if filters else self._filters
+    def _apply_filters(
+        self, value: float | None, filters: Optional[list] = None
+    ) -> float | None:
+        filters = filters if filters is not None else self._filters
         for filter in filters:
             for k, v in filter.items():
                 if k not in FILTERS:
