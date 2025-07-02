526039cb8a2d071483cf0464b82dad9c1313ff63
pszafer <pszafer@gmail.com>
2025-07-02 14:21:28 +0200

    add serving public dir

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,19 +1,32 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
   return {
-    plugins: [react()],
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
     build: {
       outDir: path.resolve(__dirname, '../boneio/webui/frontend-dist'),
       emptyOutDir: true,
     },
+    publicDir: "public",
     server: {
       proxy: {
         '/api': {
+          target: env.VITE_API_URL || 'http://localhost:8090',
+          changeOrigin: true,
+          secure: false,
+          ws: true
+        },
+        '/schema': {
           target: env.VITE_API_URL || 'http://localhost:8090',
           changeOrigin: true,
           secure: false,
