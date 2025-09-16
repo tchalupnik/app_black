85412c8add9a76a06fd3f2c999917c3095a74d38
tomaszchalupnik <tchalupnik@gmail.com>
2025-09-16 17:21:02 +0200

    Improvements

--- src/boneio/gpio_manager.py
+++ src/boneio/gpio_manager.py
@@ -3,7 +3,8 @@ from __future__ import annotations
 import asyncio
 import logging
 import os
-from collections.abc import AsyncGenerator, Callable
+from collections.abc import AsyncGenerator, Callable, Generator
+from contextlib import ExitStack, contextmanager
 from dataclasses import dataclass, field
 from datetime import timedelta
 from typing import Literal
@@ -28,17 +29,17 @@ class _Pin:
 
 @dataclass
 class GpioManager:
+    stack: ExitStack
+    chips: dict[str, gpiod.Chip] = field(default_factory=dict, init=False)
     pins: dict[str, _Pin] = field(default_factory=dict, init=False)
     _loop: asyncio.AbstractEventLoop = field(
         default_factory=asyncio.get_event_loop, init=False
     )
-    # it needs to be rewritten due to the lock is not on pin but on the chip
-    # btw breakpoint is set
-    last_value_from_callback: dict[str, bool] = field(default_factory=dict, init=False)
 
-    def __post_init__(self) -> None:
+    @classmethod
+    @contextmanager
+    def create(cls) -> Generator[GpioManager]:
         pins: dict[str, _Pin] = {}
-
         for entry in os.scandir("/dev/"):
             if not gpiod.is_gpiochip_device(entry.path):
                 continue
@@ -48,7 +49,8 @@ class GpioManager:
                     line_info = chip.get_line_info(line)
                     line_name = line_info.name.split(" ")[0]
                     pins[line_name] = _Pin(chip_path=entry.path, offset=line)
-        self.pins = pins
+        with ExitStack() as stack:
+            yield cls(stack=stack, pins=pins)
 
     def setup_input(self, pin: str, pull_mode: str = "gpio") -> None:
         """Set up a GPIO as input."""
@@ -60,47 +62,48 @@ class GpioManager:
         }.get(pull_mode)
 
         line = self.pins[pin]
-        with gpiod.Chip(line.chip_path) as chip:
-            chip.request_lines(
-                config={
-                    line.offset: gpiod.LineSettings(
-                        direction=gpiod.line.Direction.INPUT, bias=gpio_mode
-                    )
-                },
-            )
+        chip = self.chips.get(line.chip_path)
+        if chip is not None:
+            chip = self.stack.enter_context(gpiod.Chip(line.chip_path))
+
+        chip.request_lines(
+            config={
+                line.offset: gpiod.LineSettings(
+                    direction=gpiod.line.Direction.INPUT, bias=gpio_mode
+                )
+            },
+        )
 
     def write(self, pin: str, value: HIGH | LOW) -> None:
         """Write a value to a GPIO."""
-        pin_gpiod = self.pins[pin]
-        with gpiod.Chip(pin_gpiod.chip_path) as chip:
-            request = chip.request_lines(
-                config={
-                    pin_gpiod.offset: gpiod.LineSettings(
-                        direction=gpiod.line.Direction.OUTPUT
-                    )
-                },
-            )
-            request.set_value(
-                pin_gpiod.offset,
-                gpiod.line.Value.ACTIVE if value == HIGH else gpiod.line.Value.INACTIVE,
-            )
+        line = self.pins[pin]
+        chip = self.chips.get(line.chip_path)
+        if chip is not None:
+            chip = self.stack.enter_context(gpiod.Chip(line.chip_path))
+
+        request = chip.request_lines(
+            config={
+                line.offset: gpiod.LineSettings(direction=gpiod.line.Direction.OUTPUT)
+            },
+        )
+        request.set_value(
+            line.offset,
+            gpiod.line.Value.ACTIVE if value == HIGH else gpiod.line.Value.INACTIVE,
+        )
 
     def read(self, pin: str) -> bool:
         """Read a value from a GPIO."""
-        value = self.last_value_from_callback.get(pin)
-        if value is not None:
-            return value
+        line = self.pins[pin]
+        chip = self.chips.get(line.chip_path)
+        if chip is not None:
+            chip = self.stack.enter_context(gpiod.Chip(line.chip_path))
 
-        gpiod_pin = self.pins[pin]
-        with gpiod.Chip(gpiod_pin.chip_path) as chip:
-            request = chip.request_lines(
-                config={
-                    gpiod_pin.offset: gpiod.LineSettings(
-                        direction=gpiod.line.Direction.INPUT
-                    )
-                },
-            )
-            value = request.get_values()[0]
+        request = chip.request_lines(
+            config={
+                line.offset: gpiod.LineSettings(direction=gpiod.line.Direction.INPUT)
+            },
+        )
+        value = request.get_values()[0]
         return value == gpiod.line.Value.ACTIVE
 
     def add_event_callback(
@@ -127,60 +130,63 @@ class GpioManager:
     ) -> AsyncGenerator[gpiod.LineEvent, None]:
         """Add detection for RISING and FALLING events."""
         line = self.pins[pin]
-        with gpiod.Chip(line.chip_path) as chip:
-            request = chip.request_lines(
-                config={
-                    line.offset: gpiod.LineSettings(
-                        edge_detection=gpiod.line.Edge.BOTH,
-                        # debounce_period=debounce_period,
-                    )
-                },
+
+        chip = self.chips.get(line.chip_path)
+        if chip is not None:
+            chip = self.stack.enter_context(gpiod.Chip(line.chip_path))
+            self.chips[line.chip_path] = chip
+
+        request = chip.request_lines(
+            config={
+                line.offset: gpiod.LineSettings(
+                    edge_detection=gpiod.line.Edge.BOTH,
+                    # debounce_period=debounce_period,
+                )
+            },
+        )
+
+        fut = self._loop.create_future()
+
+        def c():
+            events = request.read_edge_events()
+            if not len(events):
+                return
+            self.last_value_from_callback[pin] = (
+                events[-1].event_type == gpiod.edge_event.EdgeEvent.Type.RISING_EDGE
             )
+            if fut.done():
+                return
 
+            if edge == FALLING:
+                events = [
+                    event
+                    for event in events
+                    if event.event_type == gpiod.edge_event.EdgeEvent.Type.FALLING_EDGE
+                ]
+
+            elif edge == RISING:
+                events = [
+                    event
+                    for event in events
+                    if event.event_type == gpiod.edge_event.EdgeEvent.Type.RISING_EDGE
+                ]
+
+            _LOGGER.debug(
+                "Processing %s event(s) for pin %s on edge %s.",
+                len(events),
+                pin,
+                edge,
+            )
+            fut.set_result(events)
+
+        self._loop.add_reader(request.fd, c)
+
+        while True:
+            await fut
+            result = fut.result()
+            # debounce_period is bugged
+            await asyncio.sleep(debounce_period.total_seconds())
             fut = self._loop.create_future()
-
-            def c():
-                events = request.read_edge_events()
-                if not len(events):
-                    return
-                self.last_value_from_callback[pin] = (
-                    events[-1].event_type == gpiod.edge_event.EdgeEvent.Type.RISING_EDGE
-                )
-                if fut.done():
-                    return
-
-                if edge == FALLING:
-                    events = [
-                        event
-                        for event in events
-                        if event.event_type
-                        == gpiod.edge_event.EdgeEvent.Type.FALLING_EDGE
-                    ]
-
-                elif edge == RISING:
-                    events = [
-                        event
-                        for event in events
-                        if event.event_type
-                        == gpiod.edge_event.EdgeEvent.Type.RISING_EDGE
-                    ]
-
-                _LOGGER.debug(
-                    "Processing %s event(s) for pin %s on edge %s.",
-                    len(events),
-                    pin,
-                    edge,
-                )
-                fut.set_result(events)
-
-            self._loop.add_reader(request.fd, c)
-
-            while True:
-                await fut
-                result = fut.result()
-                # debounce_period is bugged
-                await asyncio.sleep(debounce_period.total_seconds())
-                fut = self._loop.create_future()
-                for _ in result:
-                    _LOGGER.debug("add_event_callback calling callback on pin: %s", pin)
-                    callback()
+            for _ in result:
+                _LOGGER.debug("add_event_callback calling callback on pin: %s", pin)
+                callback()
